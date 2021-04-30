dcae0a6c0295d3b3d6a4f5145b50b58d6b36b742
JAVI <73225862+javiruiperez@users.noreply.github.com>
2021-04-30 08:35:00 +0200

    Add files via upload

---
+++ Trabajo final Programación (interfaz)/src/Interfaz_Principal.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Interfaz_Principal extends JFrame {
+
+    JLabel body;
+
+    public Interfaz_Principal() {
+        JPanel panelprincipal = new JPanel();
+
+        panelprincipal.add(body = new JLabel());
+        body.setText("<html><p style = 'background-image: '>Genera tus passwords seguros con un solo CLICK!</p><br><p> -Sin tener que pensar</p><br><p> -Elige de que tipo quieres que sea (Signos, mayusculas, etc.)</p><br><p> -Sencilo y rapido ¡A QUE ESPERAS!</p></html>");
+        body.setFont(new Font("Serif", Font.PLAIN, 13));
+
+
+        add(panelprincipal);
+        setSize(400, 400);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        Interfaz_Principal i = new Interfaz_Principal();
+    }
+}
+
+
