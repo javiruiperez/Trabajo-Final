5b7df8e932f4f50e3c75b7b0dac8e9ef675c0a0a
Nico <nicolasbustoos21@gmail.com>
2021-05-07 10:59:04 +0200

    Refactor Models and controllers

---
+++ main/models/Respuesta.java
@@ -0,0 +1,35 @@
+package main.models;
+
+public class Respuesta {
+    int ID_Respuesta;
+    String texto;
+
+    public Respuesta(int ID_Respuesta, String texto) {
+        this.ID_Respuesta = ID_Respuesta;
+        this.texto = texto;
+    }
+
+    public int getID_Respuesta() {
+        return ID_Respuesta;
+    }
+
+    public void setID_Respuesta(int ID_Respuesta) {
+        this.ID_Respuesta = ID_Respuesta;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    @Override
+    public String toString() {
+        return "Respuesta{" +
+                "ID_Respuesta=" + ID_Respuesta +
+                ", texto='" + texto + '\'' +
+                '}';
+    }
+}
