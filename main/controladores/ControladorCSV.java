f7a517c4405692f7c136e851b7ea069b05f39e77
Javi <jruiperezhuerta@gmail.com>
2021-05-27 11:58:53 +0200

    +

---
+++ main/controladores/ControladorCSV.java
@@ -0,0 +1,61 @@
+package main.controladores;
+
+import main.DbConnections.DBConnection;
+
+import java.sql.*;
+
+public class ControladorCSV {
+    public static int añadirEncuesta(String titulo, double remuneracion, String nombre_usuario, int numeroPreguntas){
+        Connection conn = DBConnection.getConnection();
+        String sql = "Insert Into encuestas(Titulo, Numero_Preguntas, Remuneracion, Nombre_Usuario) Values( ?, ?, ?, ?)";
+        try {
+            PreparedStatement prp = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            prp.setString(1, titulo);
+            prp.setInt(2, numeroPreguntas);
+            prp.setDouble(3, remuneracion);
+            prp.setString(4, nombre_usuario);
+            prp.executeUpdate();
+            ResultSet generatedKeys = prp.getGeneratedKeys();
+            int ID_Encuesta = 0;
+            if(generatedKeys.next()){
+                ID_Encuesta = generatedKeys.getInt(1);
+            }
+            return ID_Encuesta;
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+            return -1;
+        }
+    }
+    public static int añadirPregunta(String titulo, int ID_Encuesta){
+        Connection conn = DBConnection.getConnection();
+        String sql = "Insert Into pregunta(Texto, ID_Encuesta) Values( ?, ?)";
+        try {
+            PreparedStatement prp = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            prp.setString(1, titulo);
+            prp.setInt(2, ID_Encuesta);
+            prp.executeUpdate();
+            ResultSet generatedKeys = prp.getGeneratedKeys();
+            int ID_Pregunta = 0;
+            if(generatedKeys.next()){
+                ID_Pregunta = generatedKeys.getInt(1);
+            }
+
+            return ID_Pregunta;
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+            return -1;
+        }
+    }
+    public static void añadirRespuesta(String texto, int ID_Pregunta){
+        Connection conn = DBConnection.getConnection();
+        String sql = "Insert into respuesta(ID_Pregunta, Texto) Values( ?, ? )";
+        try {
+            PreparedStatement prp = conn.prepareStatement(sql);
+            prp.setInt(1,ID_Pregunta);
+            prp.setString(2, texto);
+            prp.executeUpdate();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+}
--- main/utils/encuestasCSV.java
+++ main/utils/encuestasCSV.java
@@ -1,6 +1,8 @@
 package main.utils;
+
 import com.csvreader.CsvReader;
 import com.csvreader.CsvWriter;
+import main.controladores.ControladorCSV;
 import main.models.Encuesta;
 import main.models.Pregunta;
 
@@ -13,87 +15,86 @@ import java.util.List;
 
 public class encuestasCSV {
 
-            public static void main(String[] args) {
-           List<Encuesta> encuestas = new ArrayList<Encuesta>();
-
-            encuestas.add(new Encuesta("encuesta de cultura",0.01,"Nikkolay21"));
-
-            ExportarCSV(encuestas);
+    public static void main(String[] args) {
 
 
-            ImportarCSV();
+        //ExportarCSV(encuestas);
+
+
+        ImportarCSV();
+    }
+
+    public static void ExportarCSV(List<Encuesta> encuestas) {
+        String salidaArchivo = "Encuesta.csv"; // Nombre del archivo
+        boolean existe = new File(salidaArchivo).exists(); // Verifica si existe
+
+        // Si existe un archivo llamado asi lo borra
+        if (existe) {
+            File archivoEncuesta = new File(salidaArchivo);
+            archivoEncuesta.delete();
         }
 
-        public static void ExportarCSV(List<Encuesta> encuestas) {
-            String salidaArchivo = "Encuesta.csv"; // Nombre del archivo
-            boolean existe = new File(salidaArchivo).exists(); // Verifica si existe
+        try {
+            // Crea el archivo
+            CsvWriter salidaCSV = new CsvWriter(new FileWriter(salidaArchivo, true), ',');
 
-            // Si existe un archivo llamado asi lo borra
-            if(existe) {
-                File archivoEncuesta = new File(salidaArchivo);
-                archivoEncuesta.delete();
-            }
+            // Datos para identificar las columnas
+            salidaCSV.write("Titulo");
+            salidaCSV.write("Remuneracion");
+            salidaCSV.write("Nombre_Usuario");
 
-            try {
-                // Crea el archivo
-                CsvWriter salidaCSV = new CsvWriter(new FileWriter(salidaArchivo, true), ',');
 
-                // Datos para identificar las columnas
-                salidaCSV.write("Titulo");
-                salidaCSV.write("Remuneracion");
-                salidaCSV.write("Nombre_Usuario");
+            salidaCSV.endRecord(); // Deja de escribir en el archivo
+
+            // Recorremos la lista y lo insertamos en el archivo
+            for (Encuesta quest : encuestas) {
+                salidaCSV.write(quest.getTitulo());
+                salidaCSV.write(String.valueOf(quest.getRemuneracion()));
+                salidaCSV.write(quest.getNombre_Usuario());
 
 
                 salidaCSV.endRecord(); // Deja de escribir en el archivo
-
-                // Recorremos la lista y lo insertamos en el archivo
-                for(Encuesta quest : encuestas ) {
-                    salidaCSV.write(quest.getTitulo());
-                    salidaCSV.write(String.valueOf(quest.getRemuneracion()));
-                    salidaCSV.write(quest.getNombre_Usuario());
-
-
-                    salidaCSV.endRecord(); // Deja de escribir en el archivo
-                }
-
-                salidaCSV.close(); // Cierra el archivo
-
-            } catch(IOException e) {
-                e.printStackTrace();
             }
-        }
 
-        public static void ImportarCSV() {
-            try{
-                List<Encuesta> encuestas = new ArrayList<Encuesta>(); // Lista donde guardaremos los datos del archivo
+            salidaCSV.close(); // Cierra el archivo
 
-                CsvReader leerEncuestas = new CsvReader("Encuesta.csv");
-                leerEncuestas.readHeaders();
-
-                // Mientras haya lineas obtenemos los datos del archivo
-                while(leerEncuestas.readRecord()) {
-                    String titulo = leerEncuestas.get(0);
-                    double Remuneracion = Double.parseDouble(leerEncuestas.get(1));
-                    String Nombre_Usuario = leerEncuestas.get(2);
-
-                    encuestas.add(new Encuesta(titulo, Remuneracion, Nombre_Usuario)); // Añade la informacion a la lista
-                }
-
-                leerEncuestas.close(); // Cierra el archivo
-
-                // Recorremos la lista y la mostramos en la pantalla
-                for(Encuesta encuesta : encuestas) {
-                    System.out.println(
-                            encuesta.getTitulo() + " , "
-                            + encuesta.getRemuneracion() + " , "
-                            + encuesta.getNombre_Usuario());
-                }
-
-            } catch(FileNotFoundException e) {
-                e.printStackTrace();
-            } catch(IOException e) {
-                e.printStackTrace();
-            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
+
+    public static void ImportarCSV() {
+        try {
+            CsvReader leerEncuestas = new CsvReader("Encuesta.csv", ',');
+
+            leerEncuestas.readRecord();
+            String titulo = leerEncuestas.get(0);
+            double remuneracion = Double.parseDouble(leerEncuestas.get(1));
+            String nombre_usuario = leerEncuestas.get(2);
+            int numero_preguntas = 0;
+            while (leerEncuestas.readRecord()) {
+                numero_preguntas++;
+            }
+            leerEncuestas.close(); //Cierra el archivo
+            int ID_Encuesta = ControladorCSV.añadirEncuesta(titulo, remuneracion, nombre_usuario, numero_preguntas); //Se crea la encuesta en la BD y se obtiene el id encuesta
+
+            leerEncuestas = new CsvReader("Encuesta.csv", ',');
+            leerEncuestas.readRecord();
+            while (leerEncuestas.readRecord()) {
+                String tituloPregunta = leerEncuestas.get(0);
+                int ID_Pregunta = ControladorCSV.añadirPregunta(tituloPregunta, ID_Encuesta);
+                String respuesta1 = leerEncuestas.get(1);//Obetenemos las respuestas
+                String respuesta2 = leerEncuestas.get(2);
+                String respuesta3 = leerEncuestas.get(3);
+                ControladorCSV.añadirRespuesta(respuesta1, ID_Pregunta);
+                ControladorCSV.añadirRespuesta(respuesta2, ID_Pregunta);
+                ControladorCSV.añadirRespuesta(respuesta3, ID_Pregunta);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
 
